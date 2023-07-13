ce12101baa82b7979a238ac6b427c399cdf51390
hien nguyen <thuhiennguyen198@gmail.com>
2023-07-13 00:24:02 -0400

    fix gradle

--- src/main/java/com/petCare/validation/ServletInitializer.java
+++ src/main/java/com/petCare/validation/ServletInitializer.java
@@ -1,13 +1,13 @@
-package com.petCare.validation;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PetworldApplication.class);
-	}
-
-}
+//package com.petCare.validation;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(PetworldApplication.class);
+//	}
+//
+//}

96ea6b7b9d89db72b3cacbb8505d9b1d6448bd1b
hien nguyen <thuhiennguyen198@gmail.com>
2023-09-09 22:22:23 -0400

    port

--- src/main/java/com/petCare/PetCareApplication.java
+++ src/main/java/com/petCare/PetCareApplication.java
@@ -11,14 +11,7 @@ public class PetCareApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(PetCareApplication.class, args);
-		String port = System.getenv("PORT");
-		if (port == null) {
-			// Default to port 8080 if PORT environment variable is not set
-			port = "8080";
-		}
-		SpringApplication app = new SpringApplication(PetCareApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", port));
-		app.run(args);
+
 	}
 
 }

124b6a171d88f2d1d1ab6d12fb3a1d598b38b95b
hien nguyen <thuhiennguyen198@gmail.com>
2023-09-09 22:17:31 -0400

    config port

--- src/main/java/com/petCare/PetCareApplication.java
+++ src/main/java/com/petCare/PetCareApplication.java
@@ -3,11 +3,22 @@ package com.petCare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class PetCareApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PetCareApplication.class, args);
+		String port = System.getenv("PORT");
+		if (port == null) {
+			// Default to port 8080 if PORT environment variable is not set
+			port = "8080";
+		}
+		SpringApplication app = new SpringApplication(PetCareApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
 
 }
